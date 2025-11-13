6a2962cf40e254c6c30440110c8547418687c019
Mithungowda M S <msmithungowda@gmail.com>
2025-11-13 19:33:30 +0530

    yes

---
+++ index1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("enter two numbeer:");
+    scanf("%d %d",&a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("after swapping: a = %d, b = %d" , a , b);
+    return 0;
+}
