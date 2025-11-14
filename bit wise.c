810cafff0ce7061d78ff2c99ec86c7928b231429
mithungowda2007 <msmithungowda@gmail.com>
2025-11-14 08:04:12 +0530

    Add files via upload

---
+++ bit wise.c
@@ -0,0 +1,14 @@
+//bit wise
+#include<stdio.h>
+int main(){
+    int a=5; //0101 in binary
+    int b=9; //1001 in binary
+
+    printf("a&b=%d\n",a&b); //AND
+    printf("a|b=%d\n",a|b); //OR
+    printf("a^b=%d\n",a^b); //XOR
+    printf("~a=%d\n",~a);
+    printf("a << 1 = %d\n", a << 1);
+    printf("a >> 1 = %d\n", a >> 1);
+    retuen 0;
+}
