c9cd4d9f13b488afcad5f9b3ef5e07909054a193
mithungowda2007 <msmithungowda@gmail.com>
2026-02-10 09:03:55 +0530

    Add files via upload

---
+++ program unit 2 c.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5]={10,25,30,45,50};
+    int i,key,found=0;
+    printf("Enter element to search:");
+    scanf("%d",&key);
+    for(i=0;i<5;i++)
+    {
+        if(arr[i]==key){
+            found=1;
+            break;
+        }
+    }
+    if(found)
+    printf("Elements %d found at index %d\n",key,i++);
+    else
+    printf("Element %d not found\n",key);
+
+}
